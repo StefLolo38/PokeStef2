b18442586eb5037f1a5be1e70bb22847975f6f9d
Coducteur <chat.adr01@gmail.com>
2023-02-05 01:42:02 +0100

    create pokemon class + array + service

---
+++ src/app/shared/Pokemon-class.ts
@@ -0,0 +1,7 @@
+export class Pokemon {
+  constructor(
+    public pokeName: string,
+    public urlImg: string,
+    public description: string
+  ) {}
+}
---
+++ src/app/shared/pokemons-mock.ts
@@ -0,0 +1,7 @@
+import { Pokemon } from './Pokemon-class';
+
+export const pokemons: Pokemon[] = [
+  new Pokemon('pok1', 'url', 'bla bla bla'),
+  new Pokemon('pok2', 'url', 'bla bla bla'),
+  new Pokemon('pok3', 'url', 'bla bla bla'),
+];
