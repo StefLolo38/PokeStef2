e9e283c4c6c089e6617a565b28daa3894ce29382
BAILLOT STEPHANE <[stefbbst@gmail.com](stefbbst@gmail.com)>
2023-03-04 17:51:17 +0100

    last commit

--- src/app/shared/pokemons-mock.ts
+++ src/app/shared/pokemons-mock.ts
@@ -16,4 +16,20 @@ export const POKEMONS: Pokemon[] = [
     'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
     'Son souffle brûlant peut faire fondre la roche. Il est parfois la cause d’incendies de forêt.'
   ),
+  new Pokemon(
+    'Stef',
+    'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png',
+  "C'est moi le plus fort!!!!"
+  ),
+  new Pokemon(
+    'Lolo',
+    'https://assets.pokemon.com/assets/cms2/img/pokedex/full/124.png',
+  "C'est moi la plus belle!!!!"
+  ),
+  new Pokemon(
+    'Alessya',
+    'https://assets.pokemon.com/assets/cms2/img/pokedex/full/013.png',
+  "C'est moi qui crie le plus fort!!!!"
+  ),
+  
 ];
